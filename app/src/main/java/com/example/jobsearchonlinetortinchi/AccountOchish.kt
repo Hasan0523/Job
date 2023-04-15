1ba63d37c6bb51d6dda158edee2a60d6c2cb54d2
PC680 <hasanboronov0523@gmail.com>
2023-04-15 10:32:19 +0500

    ghnn

--- app/src/main/java/com/example/jobsearchonlinetortinchi/AccountOchish.kt
+++ app/src/main/java/com/example/jobsearchonlinetortinchi/AccountOchish.kt
@@ -1,11 +1,20 @@
 package com.example.jobsearchonlinetortinchi
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AccountOchish : AppCompatActivity() {
+    private lateinit var button1 : Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_ochish)
+        button1 = findViewById(R.id.button)
+        button1.setOnClickListener {
+            startActivity(Intent(applicationContext, LoginToYourAccount::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/jobsearchonlinetortinchi/LoginToYourAccount.kt
@@ -0,0 +1,11 @@
+package com.example.jobsearchonlinetortinchi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginToYourAccount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_to_your_account)
+    }
+}
