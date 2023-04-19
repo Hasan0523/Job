4918503c333efb5d621a16884792569baf2fccc9
PC680 <hasanboronov0523@gmail.com>
2023-04-19 11:26:05 +0500

    trfhty

--- app/src/main/java/com/example/jobsearchonlinetortinchi/Mutaxassislik.kt
+++ app/src/main/java/com/example/jobsearchonlinetortinchi/Mutaxassislik.kt
@@ -1,11 +1,18 @@
 package com.example.jobsearchonlinetortinchi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Mutaxassislik : AppCompatActivity() {
+    private lateinit var button1 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mutaxassislik)
+        button1 = findViewById(R.id.button2)
+        button1.setOnClickListener {
+            startActivity(Intent(applicationContext, ShaxsiyMalumotingizniKiriting::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/jobsearchonlinetortinchi/ShaxsiyMalumotingizniKiriting.kt
+++ app/src/main/java/com/example/jobsearchonlinetortinchi/ShaxsiyMalumotingizniKiriting.kt
@@ -1,12 +1,11 @@
 package com.example.jobsearchonlinetortinchi
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class IshchiMutaxassisligi : AppCompatActivity() {
+class ShaxsiyMalumotingizniKiriting : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ishchi_mutaxassisligi)
+        setContentView(R.layout.activity_shaxsiy_malumotingizni_kiriting)
     }
 }
