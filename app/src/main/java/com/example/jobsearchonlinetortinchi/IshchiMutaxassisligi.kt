5beb5d6ed4d4e5c48b0833fb1783fd1527b89277
PC678 <hasanboronov0523@gmail.com>
2023-04-19 15:58:20 +0500

    hato bor

--- app/src/main/java/com/example/jobsearchonlinetortinchi/IshchiMutaxassisligi.kt
+++ app/src/main/java/com/example/jobsearchonlinetortinchi/IshchiMutaxassisligi.kt
@@ -3,10 +3,16 @@ package com.example.jobsearchonlinetortinchi
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class IshchiMutaxassisligi : AppCompatActivity() {
+    private lateinit var button1 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ishchi_mutaxassisligi)
+        button1 = findViewById(R.id.button2)
+        button1.setOnClickListener {
+            startActivity(Intent(applicationContext, ShaxsiyMalumotingizniKiriting::class.java))
+        }
     }
 }
